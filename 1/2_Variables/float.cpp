d64b9adce01010e556de07ecb164e055b6121f8b
Yadoranshi <tkfkdgodhd1@gmail.com>
2023-07-25 09:58:02 +0900

    .

--- 1/2_Variables/float.cpp
+++ 1/2_Variables/float.cpp
@@ -7,6 +7,9 @@ int main()
     float c = a - b;
 
     printf("a: %f b: %f c: %f\n", a);
+    // a: 1.000000 b: 0.000000 c: 0.000000
     printf("a/b: %f\n", a/b);
+    // a/b: 3.333333
     printf("a: %d b: %d c: %d\n", a, b, c);
+    // a: 0 b: 1073741824 c: 1610612736
 }
---
+++ 1/2_Variables/longlong.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main()
+{
+    long long int a = 10000000000;
+    int b = 10000000000;
+
+    printf("a: %lld b: %d\n", a , b);
+    //a: 10000000000 b: 1410065408
+}
