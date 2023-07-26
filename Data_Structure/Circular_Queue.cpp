79f0c3fa1529cb43d907bdd39330cef281e950f3
tkfkdgodhd1@gmail.com <tkfkdgodhd1@gmail.com>
2023-07-26 18:58:01 +0900

    .

---
+++ Data_Structure/Circular_Queue.cpp
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+typedef Circular_Queue
+{
+
+}Circular_Queue;
---
+++ Data_Structure/Queue.cpp
@@ -0,0 +1,91 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Queue
+{
+    unsigned int size;
+    unsigned int max_size;
+    int* arr;
+    int front;
+    int rear;
+}Queue;
+
+Queue* get_new_queue(int max_size = 10000)
+{
+    Queue* new_q = (Queue*)malloc(sizeof(Queue));
+    new_q->front = 0;
+    new_q->rear = 0;
+    new_q->max_size = max_size;
+    new_q->arr = (int*)malloc(sizeof(int)*max_size);
+    new_q->size = 0;
+    return new_q;
+}
+
+bool is_full(Queue* q)
+{
+    return q->size == q->max_size;
+}
+
+bool is_empty(Queue* q)
+{
+    return q->size == 0;
+}
+
+void push(Queue* q, int data)
+{
+    if(is_full(q))
+    {
+        puts("Error:: in push: queue is full");
+        exit(-1);
+    }
+    if(is_empty(q))
+    {
+        q->arr[q->front] = data;
+        q->size++;
+        return;
+    }
+    q->arr[++q->rear] = data;
+    q->size++;
+    return;
+}
+
+void pop(Queue* q)
+{
+    if(is_empty(q))
+    {
+        puts("Error:: in pop: queue is empty");
+        exit(-1);
+        return;
+    }
+    q->size--;
+    q->front++;
+}
+
+int back(Queue*q)
+{
+    if(is_empty(q))
+    {
+        puts("Error:: in pop: queue is empty");
+        exit(-1);
+    }
+    return q->arr[q->rear];
+}
+
+int front(Queue* q)
+{
+    if(is_empty(q))
+    {
+        puts("Error:: in fornt: queue is empty");
+        exit(-1);
+        return -1;
+    }
+    return q->arr[q->front];
+}
+
+int main()
+{
+    Queue* q = get_new_queue();
+    push(q, 1);
+    push(q, 2);
+    
+}
